e7704675c0e6d2975bf4c142d041f4a841673c68
Paul White <theflashofsonic@gmail.com>
2018-09-11 21:44:59 +0300

    responsive render of th field

--- utility.js
+++ utility.js
@@ -1,6 +1,15 @@
 var setDynamicStyles = function() {
+    
+    cellSize = Math.floor( ( ( innerHeight <= innerWidth )? innerHeight : innerHeight ) / N);
     d3.select('#field')
         .attr('width', innerWidth)
-        .attr('height', innerHeight);
-    cellSize = Math.floor( ( ( innerHeight <= innerWidth )? innerHeight : innerHeight ) / N);
+        .attr('height', innerHeight)
+        .selectAll('rect')
+            .data(field)
+            .attr('x', function(d) { return d.coord[1] * cellSize })
+            .attr('y', function(d) { return d.coord[0] * cellSize })
+            .attr('width', cellSize)
+            .attr('height', cellSize);
+        
+    
 }
